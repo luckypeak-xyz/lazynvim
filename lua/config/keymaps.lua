654c126ef584b2a52eec1ef168737eaada7c7dd7
ruifeng <ruifeng.yue@outlook.com>
2023-07-29 09:30:04 +0800

    Add Hop,Comment

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,3 +18,33 @@ local function map(mode, lhs, rhs, opts)
 end
 
 map("i", "jk", "<ESC>", opts)
+
+-- hop
+local hop = require("hop")
+local directions = require("hop.hint").HintDirection
+vim.keymap.set("", "f", function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, { remap = true })
+vim.keymap.set("", "F", function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, { remap = true })
+vim.keymap.set("", "t", function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR })
+end, { remap = true })
+vim.keymap.set("", "T", function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR })
+end, { remap = true })
+
+-- nvim-comment
+map(
+  "n",
+  "<leader>/",
+  "<cmd>lua require('Comment.api').toggle.linewise.current()<cr>",
+  { noremap = true, silent = true, desc = "Comment" }
+)
+map(
+  "v",
+  "<leader>/",
+  "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+  { noremap = true, silent = true, desc = "Comment" }
+)
---
+++ lua/plugins/comment.lua
@@ -0,0 +1,17 @@
+return {
+  "numToStr/Comment.nvim",
+  config = function()
+    require("Comment").setup({
+      ---Add a space b/w comment and the line
+      padding = true,
+      ---Whether the cursor should stay at its position
+      sticky = true,
+      ---Lines to be ignored while (un)comment
+      ignore = nil,
+      ---Function to call before (un)comment
+      pre_hook = nil,
+      ---Function to call after (un)comment
+      post_hook = nil,
+    })
+  end,
+}
